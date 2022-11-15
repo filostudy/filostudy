6697dcf289dea9e864004748867ed0cde02199ae
filostudy <116910891+filostudy@users.noreply.github.com>
2022-11-15 14:12:10 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const link = document.getElementById('link');
+
+const text1 = link.textContent;
+// 👇️ Visit example.com
+console.log(text1);
+
+const text2 = link.innerText;
+// 👇️ Visit example.com
+console.log(text2);
